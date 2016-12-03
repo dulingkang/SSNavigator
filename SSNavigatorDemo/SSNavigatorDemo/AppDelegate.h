570852689178e10066248437993be6ad251abb34
dulingkang@163.com <dulingkang@163.com>
2016-12-03 23:32:15 +0800

    update

---
+++ SSNavigatorDemo/SSNavigatorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSNavigatorDemo
+//
+//  Created by dulingkang on 2016/12/3.
+//  Copyright © 2016年 com.shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
